b49ead9832e9f27365e06562e0c97743350ee1fa
Vo Manh Phat <vomanhphat94@gmail.com>
2021-10-15 00:33:14 +0700

    Update version 06

--- selenium/javaTester/Topic_06_Condition_Statement.java
+++ selenium/javaTester/Topic_06_Condition_Statement.java
@@ -4,15 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Topic_06_Condition_Statement {
 	WebDriver driver;
 	String getProjectLocation = System.getProperty("user.dir");
 	String ChromeDriverLocation = getProjectLocation + "\\browserDrivers\\chromedriver.exe";
+	String FirefoxDriverLocation = getProjectLocation + "\\browserDrivers\\geckodriver.exe";
 	String IEDriverLocation = getProjectLocation + "\\browserDrivers\\IEDriverServer.exe";
+	String EdgeDriverLocation = getProjectLocation + "\\browserDrivers\\msedgedriver.exe";
 
 	public void TC_01_If() {
 		boolean status = 5 > 3;
@@ -44,7 +48,7 @@ public class Topic_06_Condition_Statement {
 		
 
 	}
-	@Test
+
 	public void TC_02_If_Else() {
 		// Ex có 2 điều kiện : Nếu đúng thì vào if - sai vào else
 		
@@ -57,13 +61,37 @@ public class Topic_06_Condition_Statement {
 
 		System.setProperty("webdriver.chrome.driver", ChromeDriverLocation);
 		driver = new ChromeDriver();
-
+		System.out.println(driver.toString());
 		if(driver.toString().contains("internet explorer")) {
 			System.out.println("Click by javascript");
 		} else {
 			System.out.println("Click by Selenium WebElement");
 		}
 	}
+	
+	@Parameters("browser")
+	@Test
+	public void TC_03_If_Else_If_Else(String browserName) {
+		if(browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", ChromeDriverLocation);
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", FirefoxDriverLocation);
+			driver = new FirefoxDriver();
+		}  else if (browserName.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", IEDriverLocation);
+			driver = new InternetExplorerDriver();
+		}	else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", EdgeDriverLocation);
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please input correct the borwser name!");
+		}
+		
+		System.out.println(driver.toString() + "  " + browserName);
+		driver.quit();
+	}
+	
 
 
 }
